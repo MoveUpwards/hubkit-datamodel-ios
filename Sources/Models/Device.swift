e9743b85daaa4a2d99ef15765e54177022f3dfe2
lgriffie <loic@griffie.fr>
2021-06-01 17:18:11 +0200

    Add device init

--- Sources/Models/Device.swift
+++ Sources/Models/Device.swift
@@ -17,16 +17,16 @@ public struct Device: Codable {
     public let externalUUID: String?
 
     /// The name of the device
-    public let name: String?
+    public let name: String
 
     /// The MAC address of the device
-    public let macAddress: String?
+    public let macAddress: String
 
     /// The hardware version of the device
-    public let hardwareVersion: String?
+    public let hardwareVersion: String
 
     /// The firmware version of the device
-    public let firmwareVersion: String?
+    public let firmwareVersion: String
 
     /// Is the device start recording data manually
     public let manualMode: Bool
@@ -38,7 +38,7 @@ public struct Device: Codable {
     public let longitude: Double?
 
     /// The device type
-    public let sensorType: String?
+    public let sensorType: String
 
     /// The device battery level in percentile
     public let battery: Int?
@@ -48,6 +48,34 @@ public struct Device: Codable {
 
     /// The device activation date
     public let activatedAt: Date?
+
+    public init(id: UUID = UUID(),
+                externalUUID: String? = nil,
+                name: String,
+                macAddress: String,
+                hardwareVersion: String,
+                firmwareVersion: String,
+                sensorType: String,
+                manualMode: Bool = false,
+                activated: Bool = false,
+                latitude: Double? = nil,
+                longitude: Double? = nil,
+                battery: Int? = nil,
+                activatedAt: Date? = nil) {
+        self.id = id
+        self.externalUUID = externalUUID
+        self.name = name
+        self.macAddress = macAddress
+        self.hardwareVersion = hardwareVersion
+        self.firmwareVersion = firmwareVersion
+        self.latitude = latitude
+        self.longitude = longitude
+        self.sensorType = sensorType
+        self.battery = battery
+        self.manualMode = manualMode
+        self.activated = activated
+        self.activatedAt = activatedAt
+    }
 }
 
 extension Device {
